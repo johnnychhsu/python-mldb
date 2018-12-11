2cc7f474f8bcd8bb5f61f4b003e8242920527e76
Jeff Lu <r06921039@ntu.edu.tw>
2018-12-11 22:53:56 +0800

    add load_from_database

--- python_mldb/Dataset.py
+++ python_mldb/Dataset.py
@@ -6,14 +6,15 @@ Created on Tue Dec 11 17:07:13 2018
 @author: jeff_lu
 """
 import pandas as pd
+import numpy as np
+
 
 class Dataset(object):
     def __init__(self, query_handler):
         self.query_handler = query_handler
-        
 
     def save_to_database(self, csv_file, table_name):
-        data = pd.read_csv(csv_file, nrows = 2)
+        data = pd.read_csv(csv_file, nrows=2)
         col_name = []
         col_type = []
         for name in data.columns:
@@ -31,12 +32,26 @@ class Dataset(object):
         sql_query = sql_query[:-1]
         sql_query += ")"
         self.query_handler.run_query(sql_query)
-        load_query = "LOAD DATA LOCAL INFILE '" + csv_file + "' INTO TABLE " + table_name
-        load_query += " FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES"
+        load_query = "LOAD DATA LOCAL INFILE '" + csv_file + "' INTO TABLE "
+        + table_name
+        load_query += " FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"
+        + "IGNORE 1 LINES"
         self.query_handler.run_query(load_query)
-        
+
     def load_from_database(self, name):
-        pass
+        select_query = "SHOW COLUMNS FROM " + name
+        self.query_handler.run_query(select_query)
+        rows = []
+        data = []
+        for item in self.query_handler.cursor:
+            rows.append(item[0])
+        select_query = "SELECT * FROM " + name
+        self.query_handler.run_query(select_query)
+        for item in self.query_handler.cursor:
+            data.append(item)
+        data = np.array(data)
+
+        return pd.DataFrame(data, columns=rows)
 
     def get_data(self):
         pass
