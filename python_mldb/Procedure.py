94e3b2da93ee3b8dbab7389bde3aa3c7b9caa6e9
Johnny <johnny.chhsu01@gmail.com>
2018-12-12 14:25:19 +0800

    procedure rf done

--- python_mldb/Procedure.py
+++ python_mldb/Procedure.py
@@ -27,17 +27,32 @@ class ClassifierProcedure(Procedure):
         self.name = name
         self.description = description
 
-    def train(self, dataset):
+    def train(self, dataset_name):
         pass
 
 
 class RFClassifierProcedure(ClassifierProcedure):
 
-    def __init__(self, name):
-        super(RFClassifierProcedure, self).__init__(name)
+    def __init__(self, name, description):
+        super(RFClassifierProcedure, self).__init__(name, description)
 
-    def train(self, dataset):
-        self._train(dataset)
+    def train(self, dataset_name, **kwargs):
+        self._train(dataset_name)
 
-    def _train(self, dataset):
+    def _train(self, dataset_name, **kwargs):
+        data = self.dataset.load_from_database(dataset_name)
+        y = data[['label']].values()
+        x = data.drop(labels=['label'], axis='columns').values()
+
+        clf = RandomForestClassifier(n_estimators=100,
+                                     max_depth=2,
+                                     random_state=0,
+                                     **kwargs)
+
+        print ("Start training random forest classifier with dataset {}.".format(dataset_name))
+        clf.fit(x, y)
+
+        self._save_to_db(clf)
+
+    def _save_to_db(self):
         pass
